bc0679e71b491f4a40756d9aa0ab838d849763e2
Your Name <you@example.com>
2023-11-19 12:45:12 +0530

    new project

--- HotelManegment_project/src/app/app.module.ts
+++ HotelManegment_project/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { AdminComponent } from './admin/admin.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
-    AdminComponent
+   
   ],
   imports: [
     BrowserModule,
