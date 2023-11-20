e830276da760d8829502b510e53c003e1aaed7c1
Your Name <you@example.com>
2023-11-20 12:34:33 +0530

    component

--- HotelManegment_project/src/app/owner/owner.module.ts
+++ HotelManegment_project/src/app/owner/owner.module.ts
@@ -5,17 +5,25 @@ import { OwnerRoutingModule } from './owner-routing.module';
 import { OwnerhomeComponent } from './ownerhome/ownerhome.component';
 import { OwnerSignupComponent } from './ownerhome/owner-signup/owner-signup.component';
 import { OwnerLoginComponent } from './ownerhome/owner-login/owner-login.component';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { SharedModule } from './ownerhome/owner-login/common/shared/shared.module';
 
 
 @NgModule({
   declarations: [
     OwnerhomeComponent,
     OwnerSignupComponent,
-    OwnerLoginComponent
+    OwnerLoginComponent,
+  
   ],
   imports: [
     CommonModule,
-    OwnerRoutingModule
+    OwnerRoutingModule,
+    SharedModule
+    
   ]
 })
-export class OwnerModule { }
+export class OwnerModule {
+  
+}
+ 
--- HotelManegment_project/src/app/owner/ownerhome/owner-login/owner-login.component.ts
+++ HotelManegment_project/src/app/owner/ownerhome/owner-login/owner-login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-owner-login',
